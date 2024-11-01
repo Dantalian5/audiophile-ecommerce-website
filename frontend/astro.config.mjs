3f94c21d832cc200c4e1e81f377d09aa5d184e39
Marcos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-11-01 15:31:55 +0100

    Add lighthouse to pacakjes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import pageInsight from "astro-page-insight";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()],
+  integrations: [sentry(), spotlightjs(), pageInsight()],
   vite: {
     css: {
       preprocessorOptions: {
