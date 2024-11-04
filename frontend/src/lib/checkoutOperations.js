9d0787b2ccb2eadb6389533c6c14fe627632a4b0
Marcos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-11-04 09:09:57 +0100

    Fixes on firefox warnings

--- frontend/src/lib/checkoutOperations.js
+++ frontend/src/lib/checkoutOperations.js
@@ -1,6 +1,7 @@
 export async function validateCart(cart) {
   try {
-    const response = await fetch(`${import.meta.env.PUBLIC_API_URL}/cart`, {
+    const baseURL = import.meta.env.PUBLIC_API_URL.replace(/\/+$/, "");
+    const response = await fetch(`${baseURL}/cart`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -22,7 +23,8 @@ export async function validateCart(cart) {
 
 export async function checkoutCart(billing, cart) {
   try {
-    const response = await fetch(`${import.meta.env.PUBLIC_API_URL}/checkout`, {
+    const baseURL = import.meta.env.PUBLIC_API_URL.replace(/\/+$/, "");
+    const response = await fetch(`${baseURL}/checkout`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
